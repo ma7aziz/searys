c9a0e32abffd9881dedbc3eb3fde0a0bda624cb0
ma7aziz <ma7.aelaziz@gmail.com>
2024-06-18 13:54:38 +0300

    nav bar fixed and transiton

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+// src/components/NavBar.js
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/logo.png';
 import { Navbar, Nav, Container, Offcanvas, Button } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import '/src/styles/NavBar.scss'
+import '../styles/NavBar.scss'; // Ensure correct path to your NavBar.scss file
+
 const NavBar = () => {
   const { t, i18n } = useTranslation();
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(false);
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) { // Adjust 56 to match your navbar height
+        console.log(window.scrollY > 600 )
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Navbar bg="light" expand="md"  aria-label="main-navigation">
+      <Navbar bg="light" expand="md" className={`navbar ${isSticky ? 'sticky' : ''}`} aria-label="main-navigation">
         <Container>
           <Navbar.Brand as={Link} to="/">
             <img loading="lazy" src={logo} alt="Logo" className="nav-logo d-inline-block align-top" />
           </Navbar.Brand>
           <Button variant="outline-primary" onClick={handleShow} className="ms-auto d-md-none">
-            <span className="navbar-toggler-icon " ></span>
+            <span className="navbar-toggler-icon"></span>
           </Button>
           <Navbar.Collapse id="basic-navbar-nav" className="d-none d-lg-flex">
             <Nav className="ms-auto nav-links">
@@ -33,7 +52,7 @@ const NavBar = () => {
         </Container>
       </Navbar>
 
-      <Offcanvas show={show} onHide={handleClose} placement="start" >
+      <Offcanvas show={show} onHide={handleClose} placement="start">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>
             <img loading="lazy" src={logo} alt="Logo" className="nav-logo" />
@@ -45,9 +64,7 @@ const NavBar = () => {
             <Nav.Link as={NavLink} className="nav-link" to="/services" onClick={handleClose}>{t('Services')}</Nav.Link>
             <Nav.Link as={NavLink} className="nav-link" to="/about" onClick={handleClose}>{t('About')}</Nav.Link>
             <Nav.Link as={NavLink} className="nav-link" to="/contact" onClick={handleClose}>{t('Contact')}</Nav.Link>
-
           </Nav>
-        
         </Offcanvas.Body>
       </Offcanvas>
     </>
