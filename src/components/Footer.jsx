95bf0a1f158cb777d5f2af16a2ea0ce8579e730e
ma7aziz <ma7.aelaziz@gmail.com>
2024-06-18 13:12:21 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,51 +1,55 @@
-// Footer.js
+// src/components/Footer.js
 import React from 'react';
 import { FiPhone, FiMail, FiFacebook, FiInstagram, FiLinkedin, FiTwitter } from 'react-icons/fi';
 
-
-const logostyle = {
-    width: "auto",
-    height: "200px",
-    aspectRatio: "1.2",
-
-}
-
 const Footer = () => {
     return (
-        <footer className="text-light  py-4 mt-5">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-md-5 my-auto">
-                        <img src="/src/assets/logo-w.png" style={logostyle} alt="Seary Ceauticals Logo" />
-                        {/* <p>
-                            Our commitment to excellence ensures the best for your products.
-                        </p> */}
-                    </div>
-                    <div className="col-md-4 my-auto">
-                        <h5 className='text-green'>Contact Us</h5>
-                        <div>
-                            <p className='d-flex align-items-center'><span className='me-2 text-green'><FiPhone /></span> <a href="tel:+20101236987" className="text-light">+20 101236987</a></p>
-                            <p><span className='text-green'><FiMail /></span> <a href="mailto:info@pharmaco.com" className="text-light">info@searyceuaticals.com</a></p>
-                        </div>
-                    </div>
-                    <div className="col-md-3 ">
-                        <h5 className='text-green'>Follow Us</h5>
-                        <div>
-                            <a href="#" className="text-light me-3"><FiFacebook /> </a>
-                            <a href="#" className="text-light me-3"><FiTwitter /></a>
-                            <a href="#" className="text-light me-3"><FiLinkedin /></a>
-                            <a href="#" className="text-light"><FiInstagram /> </a>
-                        </div>
-                    </div>
-                </div>
-
+      <footer className="footer text-light py-4 mt-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-3 my-auto text-center text-md-start">
+              <img src="/src/assets/logo-w.png" className="footer-logo" alt="Seary Ceauticals Logo" />
             </div>
-            <div className="text-center mt-3 bg-dark-primary border-top pt-3">
-                &copy; {new Date().getFullYear()} Seary Ceuticals. All rights reserved.
+            <div className="col-md-3 my-auto text-center text-md-start">
+              <h5 className='text-green'>Contact Us</h5>
+              <div>
+                <p className='d-flex align-items-center justify-content-center justify-content-md-start'>
+                  <FiPhone className='me-2 text-green' />
+                  <a href="tel:+20101236987" className="">+20 101236987</a>
+                </p>
+                <p className='d-flex align-items-center justify-content-center justify-content-md-start'>
+                  <FiMail className='me-2 text-green' />
+                  <a href="mailto:info@pharmaco.com" className="">info@searyceuticals.com</a>
+                </p>
+              </div>
             </div>
-        </footer>
-    
+            <div className="col-md-3">
+                <h6 className='text-green'>Quick Links</h6>
+                <ul className="list-unstyled">
+                  <li><a href="#" className="text-light">About Us</a></li>
+                  <li><a href="#" className="text-light">Products</a></li>
+                  <li><a href="#" className="text-light">Contact</a></li>
+                </ul>
+              </div>
+            <div className="col-md-3 text-center text-md-start">
+              <h5 className='text-green'>Follow Us</h5>
+              <div className='d-flex justify-content-center justify-content-md-start'>
+                <a href="#" className="text-light me-3 footer-icon"><FiFacebook /></a>
+                <a href="#" className="text-light me-3 footer-icon"><FiTwitter /></a>
+                <a href="#" className="text-light me-3 footer-icon"><FiLinkedin /></a>
+                <a href="#" className="text-light footer-icon"><FiInstagram /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+          <div className="mt-3 copyright">
+            &copy; {new Date().getFullYear()} Seary Ceuticals. All rights reserved.
+          </div>
+        
+      </footer>
     );
-};
-
-export default Footer;
+  };
+  
+  export default Footer;
+  
